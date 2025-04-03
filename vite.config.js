aa47e6abc3e1be65f6f64fc025479fc861994ab8
Ariyan Sidiq <aryansidiq10@gmail.com>
2025-04-03 23:34:42 +0530

    New changes are added

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import fs from 'fs'
+import https from 'https'
 
 export default defineConfig({
   plugins: [react()],
@@ -13,6 +15,10 @@ export default defineConfig({
     target: 'esnext' // Ensures support for top-level await
   },
   server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, './certs/server.key')), // Path to your SSL private key
+      cert: fs.readFileSync(path.resolve(__dirname, './certs/server.cert')), // Path to your SSL certificate
+    },
     port: 5173,
     proxy: {
       '/api': {
